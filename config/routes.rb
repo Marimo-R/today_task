2ee5fa11711cd33088f4e5f306ff72e59a02e812
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-29 13:34:14 +0000

    11/29 Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "users/registrations",
+    sessions: 'users/sessions'
+  }
+  #devise_for :users
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
