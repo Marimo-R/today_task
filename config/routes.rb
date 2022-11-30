8d62ea11b780da3da8e20feaa80994c89d394b74
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-11-30 13:28:08 +0000

    [Finish]add-user-function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     sessions: 'users/sessions'
   }
-  #devise_for :users
+  
+  get 'users/my_page' => 'users#show'
+  get 'users/information/edit' => 'users#edit'
+  patch 'users/information' => 'users#update'
+  get 'users/unsubscribe' => 'users#unsubscribe'
+  patch 'users/withdrawal' => 'users#withdrawal'
   root to: 'homes#top'
   get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
