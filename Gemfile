66032887db7a91b538d487f7769f2992c2ccbe68
Marimo-R <113738982+Marimo-R@users.noreply.github.com>
2022-12-19 09:28:33 +0000

    [Finish] add basic calendar-function

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'enum_help'
+gem "simple_calendar", "~> 2.0"
+gem 'rails-i18n'
--- app/models/main_task.rb
+++ app/models/main_task.rb
@@ -2,5 +2,6 @@ class MainTask < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :sub_tasks, dependent: :destroy
+  validates :main_task, presence: true
   enum status: { incomplete: 0, done: 1, deleted: 2 }
 end
